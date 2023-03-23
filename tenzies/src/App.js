e20d4e634d05c82d6e2b4d7391d090d51141f9e6
yuval2017 <hitter@post.bgu.ac.il>
2023-03-23 23:19:02 +0200

    rool only hele cubes

--- tenzies/src/App.js
+++ tenzies/src/App.js
@@ -7,19 +7,30 @@ function App() {
   const [numOfDices, setNumOfDices] = React.useState(10)
   const [dices, setDices] = React.useState(allNewDice())
 
+function generateNewDice(id){
+ return {
+    value: Math.ceil(Math.random() * 6) , isHeld: false, id: id
+  }
+}
 
 function allNewDice(){
   const currDices = []
   for (let i = 0; i < numOfDices; i++){
-    currDices.push({value: Math.ceil(Math.random() * 6) , isHeld: false, id: nanoid()})
+    currDices.push(generateNewDice(nanoid()))
   }
   return currDices
 }
-function rollDice(){
-  setDices(allNewDice())
+function onToggle(id){
+  return () =>
+    setDices(preveDices => preveDices.map(dice => dice.id === id? {...dice ,isHeld :!dice.isHeld} : dice))
 }
+
+function rollDice(){
+  setDices(prevDices => prevDices.map(dice => !dice.isHeld ? generateNewDice(dice.id) : dice))
+}
+
 const diceElements = dices.map(dice => 
-  <Die key = {dice.id} value = {dice.value}/>)
+  <Die key = {dice.id} value = {dice.value} isHeld = {dice.isHeld} onToggle = {onToggle(dice.id)}/>)
 
   return (
     <main>
--- tenzies/src/components/Die.js
+++ tenzies/src/components/Die.js
@@ -2,9 +2,12 @@ import React from "react";
 import '../style.css'
 
 export function Die(props){
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"}
     return (
+
         //nested elemetns
-        <div className="die-face">
+        <div className="die-face" style = {styles} onClick = {props.onToggle}>
             <h2 className="die-num">{props.value}</h2>
         </div>
     )
